07ac041e4084054f25154b33121fc8b581d1779c
jscavetta95 <jscavetta95@gmail.com>
2018-03-08 11:56:51 -0500

    Added MySQL connection and updated Driver to store the BPI from yesterday and store the analysis results of sample tweets.

--- BitcoinTweetAnalyzer/src/controller/Driver.java
+++ BitcoinTweetAnalyzer/src/controller/Driver.java
@@ -1,6 +1,9 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -14,56 +17,51 @@ import controller.TextAnalyzer.SentimentMode;
 
 public class Driver
 {
-	public static void main(String[] args) throws TextAPIException, JSONException, IOException
-	{
+	public static void main(String[] args) throws TextAPIException, JSONException, IOException, SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException
+	{		
+		Connection database = 
+				DriverManager.getConnection("jdbc:mysql://localhost:3306/bitcoin_price_sentiment", 
+											"root", "root");
+		
 		// Testing text analyzer.
-		testTextAnalysis();
+		//testTextAnalysis();
 		
-		// Testing BPI retriever
+		// Insert BPI from yesterday into database.
 		double bpi = CoinDeskAPI.getBPIForYesterday();
-		System.out.println("BPI for yesterday: " + bpi);
-		System.out.println();
-
-		ArrayList<Double> bpiList = CoinDeskAPI.getBPIFromDateRange(LocalDate.of(2017, 12, 15), LocalDate.of(2017, 12, 20));
+		String sql = String.format("INSERT INTO bitcoin_price_index (price_index, date) VALUES (%s, '%s')", bpi, LocalDate.now());
 		
-		System.out.println("BPI from 12/15/17 to 12/20/17");
-		int day = 15;
-		for(double bpiValue : bpiList) 
+		//database.createStatement().executeUpdate(sql);
+		
+		// Insert sentiments from sample tweets into the database.
+		ArrayList<String> tweetSamples = loadSamples();
+		
+		Sentiment sentimentResult;
+		
+		for(String sample : tweetSamples) 
 		{
-			System.out.println(day + "th: " + bpiValue);
-			day++;
+			sentimentResult = TextAnalyzer.analyzeSentiment(sample, SentimentMode.TWEET);
+			
+			sql = String.format("INSERT INTO sentiment (polarity, polarity_confidence, date) VALUES ('%s', %s, '%s')",
+								sentimentResult.getPolarity(), sentimentResult.getPolarityConfidence(), LocalDate.now());
+			
+			database.createStatement().executeUpdate(sql);
 		}
 	}
 	
-	private static void testTextAnalysis() throws TextAPIException
+	private static ArrayList<String> loadSamples() throws TextAPIException
 	{
-		Sentiment sentimentResult;
-
-		String sampleTweet = "The last week has shown beyond all doubt that "
-				+ "Bitcoin is anything but a \"safe asset\". It's a highly risky speculative investment. "
-				+ "Can we stop the \"digital gold\" nonsense now, please?";
-		sentimentResult = TextAnalyzer.analyzeSentiment(sampleTweet, SentimentMode.TWEET);
-		System.out.println(sampleTweet + "\n\t " + sentimentResult.getPolarity() + ", "
-				+ sentimentResult.getPolarityConfidence() + "\n");
-
-		sampleTweet = "Bitcoin mining is potentially even more wasteful of energy than gold mining";
-		sentimentResult = TextAnalyzer.analyzeSentiment(sampleTweet, SentimentMode.TWEET);
-		System.out.println(sampleTweet + "\n\t " + sentimentResult.getPolarity() + ", "
-				+ sentimentResult.getPolarityConfidence() + "\n");
-
-		sampleTweet = "Winklevii: Bitcoin could be worth 40 times what it is right now";
-		sentimentResult = TextAnalyzer.analyzeSentiment(sampleTweet, SentimentMode.TWEET);
-		System.out.println(sampleTweet + "\n\t " + sentimentResult.getPolarity() + ", "
-				+ sentimentResult.getPolarityConfidence() + "\n");
-
-		sampleTweet = "Arizona may allow people to pay their taxes with bitcoin http://nyp.st/2E9TvDM  via @nypost";
-		sentimentResult = TextAnalyzer.analyzeSentiment(sampleTweet, SentimentMode.TWEET);
-		System.out.println(sampleTweet + "\n\t " + sentimentResult.getPolarity() + ", "
-				+ sentimentResult.getPolarityConfidence() + "\n");
-
-		sampleTweet = "Cryptocurrency bull run imminent with bitcoin to hit $50,000 in 2018";
-		sentimentResult = TextAnalyzer.analyzeSentiment(sampleTweet, SentimentMode.TWEET);
-		System.out.println(sampleTweet + "\n\t " + sentimentResult.getPolarity() + ", "
-				+ sentimentResult.getPolarityConfidence() + "\n");
+		ArrayList<String> samples = new ArrayList<String>();
+		
+		samples.add("The last week has shown beyond all doubt that "
+				  + "Bitcoin is anything but a \"safe asset\". It's a highly risky speculative investment. "
+				  + "Can we stop the \"digital gold\" nonsense now, please?");
+		
+		samples.add("Bitcoin mining is potentially even more wasteful of energy than gold mining");
+		
+		samples.add("Winklevii: Bitcoin could be worth 40 times what it is right now");
+		
+		samples.add("Cryptocurrency bull run imminent with bitcoin to hit $50,000 in 2018");
+		
+		return samples;
 	}
 }
